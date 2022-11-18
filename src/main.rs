8c976abae906d077ef443dcfb71c93e81773e2c9
Nicolai Frigaard <nicolai.frigaard@gmail.com>
2022-11-18 13:20:40 +0100

    feat: use lexer tolkens for interpreter

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,9 @@
 use std::io::Read;
 
+use brainfuck_lexer::{error::LexerError, lex, Block, Tolken};
 use clap::Parser;
 
 const HEAP_SIZE: usize = 30_000;
-
-const INCREMENT: char = '+';
-const DECREMENT: char = '-';
-const NEXT_CELL: char = '>';
-const PREV_CELL: char = '<';
-const PRINT: char = '.';
-const INPUT: char = ',';
-const LOOP_BEGIN: char = '[';
-const LOOP_END: char = ']';
 
 #[derive(Parser)]
 struct Args {
@@ -21,14 +13,19 @@ struct Args {
 #[derive(Debug)]
 enum BrainfuckError {
     IOError(std::io::Error),
-    UnexpectedEOF,
+    ParserError(LexerError),
     ReadError,
-    SyntaxError(char),
 }
 
 impl From<std::io::Error> for BrainfuckError {
     fn from(e: std::io::Error) -> Self {
         Self::IOError(e)
+    }
+}
+
+impl From<LexerError> for BrainfuckError {
+    fn from(e: LexerError) -> Self {
+        Self::ParserError(e)
     }
 }
 
@@ -57,72 +54,42 @@ fn get_u8_from_stdin() -> Option<u8> {
         .map(|byte| byte as u8)
 }
 
-fn get_op_from_src_raw(src: &String, pc: usize) -> Option<char> {
-    src.chars().nth(pc)
-}
-
-fn get_op_from_src(src: &String, pc: usize) -> Result<char, BrainfuckError> {
-    get_op_from_src_raw(src, pc).map_or_else(|| Err(BrainfuckError::UnexpectedEOF), |v| Ok(v))
-}
-
 fn main() -> Result<(), BrainfuckError> {
     let args = Args::parse();
-    let code = get_source_as_str(args.src)?;
+    let src = get_source_as_str(args.src)?;
+    let code = lex(src)?;
     brainfuck(code)
 }
 
-fn brainfuck(src: String) -> Result<(), BrainfuckError> {
-    let mut pc = 0;
+fn brainfuck(src: Block) -> Result<(), BrainfuckError> {
     let mut memory = [0u8; HEAP_SIZE];
     let mut ptr = 0;
-    let mut stack = vec![];
 
-    while pc < src.len() {
-        let op = get_op_from_src(&src, pc)?;
+    interpret_block(&src, &mut memory, &mut ptr)
+}
 
+fn interpret_block(
+    block: &Block,
+    memory: &mut [u8; HEAP_SIZE],
+    ptr: &mut usize,
+) -> Result<(), BrainfuckError> {
+    for op in block {
         match op {
-            INCREMENT => memory[ptr] = memory[ptr].wrapping_add(1),
-            DECREMENT => memory[ptr] = memory[ptr].wrapping_sub(1),
-            NEXT_CELL => ptr = (ptr.wrapping_add(1)) % memory.len(),
-            PREV_CELL => ptr = (ptr.wrapping_sub(1)) % memory.len(),
-            PRINT => print!("{}", memory[ptr] as char),
-            INPUT => {
-                memory[ptr] =
+            Tolken::Increment => memory[*ptr] = memory[*ptr].wrapping_add(1),
+            Tolken::Decrement => memory[*ptr] = memory[*ptr].wrapping_sub(1),
+            Tolken::Next => *ptr = ptr.wrapping_add(1) % memory.len(),
+            Tolken::Prev => *ptr = ptr.wrapping_sub(1) % memory.len(),
+            Tolken::Print => print!("{}", memory[*ptr] as char),
+            Tolken::Input => {
+                memory[*ptr] =
                     get_u8_from_stdin().map_or_else(|| Err(BrainfuckError::ReadError), |v| Ok(v))?
             }
-            LOOP_BEGIN => {
-                if memory[ptr] != 0 {
-                    stack.push(pc);
-                } else {
-                    let mut depth = 1;
-                    loop {
-                        pc += 1;
-
-                        match get_op_from_src(&src, pc)? {
-                            LOOP_BEGIN => depth += 1,
-                            LOOP_END => depth -= 1,
-                            _ => (),
-                        }
-
-                        if depth == 0 {
-                            break;
-                        }
-                    }
+            Tolken::Closure(block) => {
+                while memory[*ptr] != 0 {
+                    interpret_block(block, memory, ptr)?;
                 }
             }
-            LOOP_END => {
-                pc = stack
-                    .pop()
-                    .map_or_else(|| Err(BrainfuckError::SyntaxError(']')), |v| Ok(v))?
-                    - 1;
-            }
-            #[cfg(not(feature = "comments"))]
-            c @ _ => Err(BrainfuckError::SyntaxError(c))?,
-            #[cfg(feature = "comments")]
-            _ => (),
         }
-
-        pc += 1;
     }
 
     Ok(())
