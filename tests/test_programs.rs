df5057b2c17eba6545efa05d2aa29bec187de9a8
Nicolai Frigaard <nicolai.frigaard@gmail.com>
2022-11-22 12:04:35 +0100

    feat: add integration tests for input

--- tests/test_programs.rs
+++ tests/test_programs.rs
@@ -1,4 +1,6 @@
-use brainfuck_interpreter::interpreter::brainfuck;
+use std::io::Cursor;
+
+use brainfuck_interpreter::interpreter::interpret;
 use brainfuck_lexer::lex;
 
 #[test]
@@ -9,9 +11,42 @@ fn hello_world() {
     assert!(bf.is_ok());
 
     let mut buf = Vec::new();
-    let res = brainfuck(bf.unwrap(), &mut buf);
+    let mut input = Cursor::new(vec![0u8]);
+    let res = interpret(&bf.unwrap(), &mut input, &mut buf);
     assert!(res.is_ok());
 
     let str: String = buf.into_iter().map(|v| v as char).collect();
     assert_eq!(str, "Hello World!\n".to_string());
+}
+
+#[test]
+fn cat_char() {
+    let src = ",.".to_string();
+    let bf = lex(src);
+
+    assert!(bf.is_ok());
+
+    let mut buf = Vec::new();
+    let mut input = Cursor::new(vec![b'A']);
+    let res = interpret(&bf.unwrap(), &mut input, &mut buf);
+    assert!(res.is_ok());
+
+    let str: String = buf.into_iter().map(|v| v as char).collect();
+    assert_eq!(str, "A".to_string());
+}
+
+#[test]
+fn cat_string() {
+    let src = ",[.,]".to_string();
+    let bf = lex(src);
+
+    assert!(bf.is_ok());
+
+    let mut buf = Vec::new();
+    let mut input = Cursor::new("This is the way".as_bytes());
+    let res = interpret(&bf.unwrap(), &mut input, &mut buf);
+    assert!(res.is_ok());
+
+    let str: String = buf.into_iter().map(|v| v as char).collect();
+    assert_eq!(str, "This is the way".to_string());
 }
